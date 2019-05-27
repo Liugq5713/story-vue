f6131c422f6704612a86cc480556d3b8f3060dbb
liugq <liugq5713@gmail.com>
2019-05-27 10:56:44 +0800

    ci

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,17 @@ set -e
 
 # 生成静态文件
 npm run docs:story
+
+cd ./docs
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果发布到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+git push -f  "https://${access_token}@github.com/Liugq5713/components.git" master:gh-pages
+
+cd -
