7453b182f87879dbebf25b870cd044b1174baf3d
liugq5713 <liugq5713@gmail.com>
2019-05-28 08:26:14 +0800

    todo

--- src/components/Drawer/drawer.stories.js
+++ src/components/Drawer/drawer.stories.js
@@ -1,41 +1,57 @@
 /* eslint-disable no-unused-vars */
 // eslint-disable-next-line no-unused-vars
-import Vue from 'vue'
-import { storiesOf } from '@storybook/vue'
-import { withKnobs, text, select } from '@storybook/addon-knobs'
-import Drawer from './index'
-import note from './README.md'
+import Vue from "vue";
+import { storiesOf } from "@storybook/vue";
+import { withKnobs, text, select } from "@storybook/addon-knobs";
+import addons from "@storybook/addons";
+import Events from "@storybook/core-events";
+import Drawer from "./index";
+import note from "./README.md";
 
-storiesOf('Drawer', module)
+storiesOf("Drawer", module)
   .addDecorator(withKnobs)
   .add(
-    'without props',
+    "without props",
     () => ({
       components: { Drawer },
-      template: '<Drawer></Drawer>'
+      template: "<Drawer></Drawer>"
     }),
     {
       notes: note
     }
   )
-  .add('custom position', () => ({
+  .add("custom position", () => ({
     components: { Drawer },
     template: '<Drawer position="top"></Drawer>'
   }))
-  .add('custom content', () => {
+  .add("custom content", () => {
     const positions = {
-      Top: 'top',
-      Right: 'right',
-      Left: 'left',
-      Bottom: 'bottom'
-    }
+      Top: "top",
+      Right: "right",
+      Left: "left",
+      Bottom: "bottom"
+    };
     return {
       components: { Drawer },
       props: {
         position: {
-          default: select('Position', positions, 'top')
+          default: select("Position", positions, "top")
         }
       },
-      template: '<Drawer :position="position"></Drawer>'
-    }
-  })
+      watch: {
+        position(val) {
+          console.log("change", val);
+          this.$forceUpdate();
+          addons.getChannel().emit(Events.FORCE_RE_RENDER);
+        }
+      },
+
+      template: '<Drawer :position="position"></Drawer>',
+      created() {
+        console.log("------created");
+      },
+      destroyed() {
+        console.log("---destroyed");
+      }
+    };
+  });
